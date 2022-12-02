2319e3dfd945d1dbdc6e7d8ed292564b1247b357
Atul Paswan <90141410+paswanatul471@users.noreply.github.com>
2022-12-02 15:12:13 +0530

    no changes
    no changes in App.js file.  I'm just  checking my local directory  is running or not.

--- src/Layout.js
+++ src/Layout.js
@@ -2,24 +2,9 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
   return (
     <>
-      {/* <nav >
-          <ul className="nav-bar">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/blogs">Brands</Link>
-            </li>
-            <li>
-              <Link to="/contact">FAQs</Link>
-            </li>
-          </ul>
-        </nav> */}
-
-
       <nav className="navbar navbar-expand-lg nav-bar-clr">
         <div className="container">
-          <a className="navbar-brand" href="/hedder"><img src='https://static.momspresso.com/mymo/mymo-logo2.jpg'></img></a>
+          <a className="navbar-brand" href="/"><img src='https://static.momspresso.com/mymo/mymo-logo2.jpg'></img></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
